31b794cec53fe15253d4c72997c044062eba51dc
caigk <caigk@sina.com>
2019-10-26 03:42:16 +0800

    update

--- xampp-dev/Install.sh
+++ xampp-dev/Install.sh
@@ -1,2 +1,12 @@
 # 命令安装
 
+#安装工具
+yum -y install wget
+yum -y install net-tools
+
+#
+wget https://excellmedia.dl.sourceforge.net/project/xampp/XAMPP%20Linux/7.3.10/xampp-linux-x64-7.3.10-1-installer.run
+
+chmod a+x xampp-linux-x64-7.3.10-1-installer.run
+./xampp-linux-x64-7.3.10-1-installer.run --mode unattended
+
---
+++ xampp-dev/README.md
@@ -0,0 +1,7 @@
+# xampp-dev
+
+[xampp地址](http)
+
+## docker常用命令
+
+docker run -p 8020:8000 -v $(pwd):/var/local/www  -i -t --name mytest2 mytest:latest
